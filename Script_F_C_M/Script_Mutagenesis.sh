feb0b12ad5506f98a4ded68709cf063a504f32d6
quan <quannz6666@gmail.com>
2025-01-23 11:18:39 +0100

    Script

--- Script_F_C_M/Script_Mutagenesis.sh
+++ Script_F_C_M/Script_Mutagenesis.sh
@@ -2,21 +2,24 @@ PRUNECEL_DIR="/JavaProjects"
 
 echo $(date --iso-8601) " Starting script.. !!"
 
-cd "$PRUNECEL_DIR" || { echo "Failed to change directory to $PRUNECEL_DIR. Exiting."; exit 1; }
+cd "$PRUNECEL_DIR" || { echo "Failed to change directory to $TENTRIS_DIR. Exiting."; exit 1; }
 # Run Script
 
 java -cp target/prune-cel-0.0.1-SNAPSHOT.jar org.dice_research.cel.PruneCEL_CLI \
-    --sparqlUrl http://localhost:9050/sparql \
+    --sparqlUrl http://localhost:9030/sparql \
     --ontology ALC \
     --accuracyfunction 0 \
     --punishLongExpression true \
     --avoidPickySolutionsDecorator true \
     --iteration 0 \
-    --time 600000 \
+    --time 60000 \
     --recursive true \
     --skipNone true \
-    --inputFile ./././././T_F_Json/QALD9_plus_dbpedia/TandF_MST5_reverse.json \
-    --outputFile ./././././Results/QALD9plus-dbpedia/MST5_reverse/MST5_reverse_110.csv \
+    --inputFile ./././././T_F_Json/Mutagenesis/lps.json \
+    --outputFile ./././././Results/Mutagenesis/Mutagenesis110.csv \
     --cluster false \
     --folds 1 \
-    --foldTrainTestSavePath Fold/QALD10_MST5
+    --foldTrainTestSavePath Fold/Mutagenesis
+
+
+
